93f335068faf586c3160acd33fa57f2966e9c053
Chun-Ju <47974211+Chun-Ju@users.noreply.github.com>
2022-09-05 04:39:12 -0400

    dealing with the command inputed by user and post the semaphore as needed
    Adjustment
    (1)deal with the command inputed by user, it can be classified into 4 condition
       First.  string length more than 1: that means user must enter the wrong command,
               so dump the error message and close the program with error code
       Second. command'a': means customers arrive, so post the semaphore that means somebody arrive
       Third.  command'c': means need to close the store, so post (1 + BARBER_COUNT) semaphore that
               means the store and barbers need to close or sleep.but remember also post 1 the semaphore
               that means somebody arrives to dispatch a people who tell the store to close.
       Fourth. other:      means erro command, dump the errror message and close the program.

--- command.c
+++ command.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <semaphore.h>
 #include <fcntl.h>
 #include "defineConstant.h"
@@ -9,6 +10,7 @@ main(){
    /* -------------------------  *
     * Initialization by Felicia  *
     * -------------------------  */
+   _Bool open = TRUE;
    int result = SUCCESS;
 
    // opening the IPC semaphore by Felicia
@@ -24,10 +26,36 @@ main(){
       result = ERR_SEM_OPEN;
       goto error_handling_c0;
    }
+   /* ------------------------------------------------   *
+    * receiving the comamnd inputed by user by Felicia   *
+    * ------------------------------------------------   */
+   while (open) {
+      char  command[COMMAND_LEN];
+      printf("Command:");
+      scanf(" %s", command);
+      if (strlen(command) != 1) { //command too long
+         printf("Error(command format): it's too long\n");
+         result = ERR_ARGS;
+         goto error_handling_c1;
+      } else if (command[0] == 'a') { //customers arrive
+         sem_post(SEM_arrive);
+      } else if (command[0] == 'c'){ //signal to close the store and notify barbers to sleep
+         for(int  i = 0; i < 1 + BARBER_COUNT; i++){//1 for close store, others for barber
+            sem_post(SEM_closeAll);
+         }
+         sem_post(SEM_arrive);
+         break;
+      } else {
+         printf("Error(command format): error cpmmand, please enter a or c and enter only\n");
+         result = ERR_ARGS;
+         goto error_handling_c1;
+      }
+   }
 
    /* ------------------------------------------------------   *
     * closing and unlinking the opening semaphore by Felicia   *
     * ------------------------------------------------------   */
+error_handling_c1:
    sem_close(SEM_closeAll);
    sem_unlink("SEM_closeAll");
 error_handling_c0:

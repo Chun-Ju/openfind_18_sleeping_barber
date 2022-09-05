2f5ccf9d9ac60e578c6123d64ec7f0cc047d14f5
Chun-Ju <47974211+Chun-Ju@users.noreply.github.com>
2022-09-05 02:39:41 -0400

    write the define and error code mentioned in design file into defineConstant.h

---
+++ defineConstant.h
@@ -0,0 +1,17 @@
+#ifdef   _DEFINECONSTANT_H_
+#define  _DEFINECONSTANT_H_
+
+#define  BARBER_COUNT   3
+#define  CHAIR_COUNT    10
+
+#define  COMMAND_LEN    256
+
+#define  TRUE           1
+#define  FALSE          0
+
+#define  SUCCESS        0
+#define  ERR_BASE       0
+#define  ERR_SEM_OPEN   (ERR_BASE-1)
+#define  ERR_ARGS       (ERR_BASE-2)
+
+#endif
